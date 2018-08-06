165df594233629eb00e8bd1f24a0dc68ad7d07bf
Leudi   Rosario <lrosario@rd.magycorp.com>
2018-08-06 11:22:26 -0400

    removing nodem_odules

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import Server from "./src/server/server";
 import router from "./src/router/router";
+import Mysql from "./src/mysql/mysql";
 
 const server = Server.init(3000);
 server.app.use(router);
+
+const mysql = new Mysql();
 
 server.start(() => {
   console.log("Servidor runnin on port 3000");
